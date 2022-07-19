46d5f81607856ad36a132a2f403910aabb4267c5
Tchessi Pre <tchesspreza@yahoo.fr>
2022-07-19 19:18:50 +0200

    Premier commit

---
+++ src/component/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Home = () => {
+	return (
+		<div className="hero">
+			<div class="card bg-dark text-white border-0">
+				<img
+					src="/assets/bg3.jpg"
+					class="card-img"
+					alt="Background image"
+					height=""
+				/>
+				<div class="card-img-overlay d-flex">
+					<div className="container">
+						<h5 class="card-title display-3 fw-bolder mb-0 ">
+							NOUVELLES COLLECTION
+						</h5>
+						<p class="card-text lead fs-2">DÉCOUVREZ LES TENDANCES</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Home;
